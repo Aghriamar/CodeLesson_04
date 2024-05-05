029e17553aae1d11489230321f2897989e0fa445
Aghriamar <A1exXx88@inbox.ru>
2024-05-05 12:00:55 +0300

    Added homework_04

--- CodeLesson_04/CodeLesson_04.cpp
+++ CodeLesson_04/CodeLesson_04.cpp
@@ -1,6 +1,33 @@
 ﻿#include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+const int N = 20;
+
+void printNumbers(bool even, int N) {
+    std::cout << "Numbers: ";
+    for (int i = 0; i <= N; i++) {
+        if ((i % 2 == 0 && even) || (i % 2 != 0 && !even)) {
+            std::cout << i << " ";
+        }
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    std::cout << "Even numbers from 0 to " << N << ": ";
+    for (int i = 0; i <= N; i += 2) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Odd numbers from 0 to " << N << ": ";
+    for (int i = 1; i <= N; i += 2) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+    printNumbers(true, N);
+
+    printNumbers(false, N);
+
+    return 0;
 }
